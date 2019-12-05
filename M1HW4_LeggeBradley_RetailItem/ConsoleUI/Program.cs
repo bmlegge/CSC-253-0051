a228b3d669315bb6b9342ef8c464611de742b91a
bmlegge <36506641+bmlegge@users.noreply.github.com>
2019-12-05 02:41:26 -0500

    Add files via upload

---
+++ M1HW4_LeggeBradley_RetailItem/ConsoleUI/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RetailItem;
+/**
+ * 9/1/2019
+ * CSC 253
+ * Bradley Legge
+ * This program holds information about retail items in a class.  The information is then stored into an array and
+ * displayed to the user.
+ */
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int SIZE = 3;     //assign array size to 3.
+
+            //create a new object array
+            Items[] itemArray = new Items[SIZE];
+
+            //assign values to each array.
+            itemArray[0] = new Items("Jacket", 12, 59.95m); 
+            itemArray[1] = new Items("Jeans", 40, 34.95m);
+            itemArray[2] = new Items("Shirt", 20, 24.95m);
+
+            //table headings
+            Console.WriteLine($"\t Description \t Units on Hand \t Price");
+
+            //for loop to step through each element of the array.
+            for (int i = 0; i < itemArray.Length; i++)
+            {
+                Console.WriteLine($"Item {i+1} {itemArray[i].Description, 10:0} {itemArray[i].UnitsOnHand, 15:0}" +
+                                  $"{itemArray[i].Price, 14:C}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ M1HW4_LeggeBradley_RetailItem/RetailItem/Items.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetailItem
+{
+    public class Items
+    {
+        //fields
+        private string _description;
+        private int _unitsOnHand;
+        private decimal _price;
+
+        //default constructor
+        public Items()
+        {
+            _description = null;
+            _unitsOnHand = 0;
+            _price = 0m;
+        }
+
+        //constructor
+        public Items(string description, int unitsOnHand, decimal price)
+        {
+            _description = description;
+            _unitsOnHand = unitsOnHand;
+            _price = price;
+        }
+
+        //Description property
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+        
+        //UnitsOnHand property
+        public int UnitsOnHand
+        {
+            get { return _unitsOnHand; }
+            set { _unitsOnHand = value; }
+        }
+
+        //Price property
+        public decimal Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+    }
+}
