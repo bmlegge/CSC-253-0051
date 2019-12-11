5a5cf35b104dd6d0d97ddf33c0577eae5c8f8ac9
bmlegge <36506641+bmlegge@users.noreply.github.com>
2019-12-10 19:04:01 -0500

    Add files via upload

---
+++ M6HW2_LeggeBradley_HourlyPaySorter/WindowsFormsUI/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void employeeBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.employeeBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.employeeDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'employeeDataSet.Employee' table. You can move, or remove it, as needed.
+            this.employeeTableAdapter.Fill(this.employeeDataSet.Employee);
+
+        }
+
+        private void ascendingOrderButton_Click(object sender, EventArgs e)
+        {
+            this.employeeTableAdapter.FillByHourlyPayRate(this.employeeDataSet.Employee);
+        }
+
+        private void descendingOrderButton_Click(object sender, EventArgs e)
+        {
+            this.employeeTableAdapter.FillByHourlyPayRateDesc(this.employeeDataSet.Employee);
+        }
+    }
+}
